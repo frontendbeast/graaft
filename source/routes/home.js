c994112727b41232ff06c4c0d4d07bcc0643b726
Darren Hall <darren@darrenhall.co>
2017-09-09 23:03:00 +0100

    chore: remove old unnecessary services

--- source/routes/home.js
+++ source/routes/home.js
@@ -1,9 +1,6 @@
 'use strict'
 
 module.exports = function(app) {
-	const homeMapper = require('../services/mappers/home-mapper');
-	const homeHandler = require('../services/handlers/home-handler')(homeMapper);
-
 	// Get home page
-	app.get('/', homeHandler.get);
+	app.get('/');
 };
